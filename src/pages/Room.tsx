0e455f7ee1783c98a365739c3786e8cdcd2021f4
BOLT4L <Eyobwasihun401@gmail.com>
2025-09-19 22:27:33 +0300

    asd

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -894,7 +894,7 @@ const isPreviouslyCalled = previouslyCalledNumbers.includes(num);
     )}
 
     {/* Auto Bet Toggle Button → only visible if bet is active */}
-    {autoCard && displayedCard?.claimed && (
+    {autoCard && isBetActive && displayedCard.claimed && (
   <button
     onClick={async () => {
       if (!displayedCard || !currentRoom) return;
@@ -926,7 +926,6 @@ const isPreviouslyCalled = previouslyCalledNumbers.includes(num);
       : `${t("set_auto_bet")} card:${displayedCard?.serialNumber}`}
   </button>
 )}
-
 
   </div>
 ) : (

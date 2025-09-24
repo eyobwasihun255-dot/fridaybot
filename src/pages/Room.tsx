5972be45238969a60b14000abda9157d3f9f4cc1
BOLT4L <Eyobwasihun401@gmail.com>
2025-09-24 09:39:37 +0300

    bossmove

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -230,7 +230,7 @@ React.useEffect(() => {
     // Tiny delay so the user sees the numbers marked before claiming
     setTimeout(() => {
       handleBingoClick();
-    }, 1000); // 0.5s delay
+    }, 2000); // 0.5s delay
   }
 }, [displayedCalledNumbers, displayedCard, autoCard, currentRoom?.gameStatus, hasAttemptedBingo]);
 

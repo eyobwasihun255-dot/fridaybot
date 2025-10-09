8f9d3e480fe3a88a697aedea0f12685b78d47410
BOLT4L <Eyobwasihun401@gmail.com>
2025-10-09 15:41:16 +0300

    b

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -987,7 +987,8 @@ const Room: React.FC = () => {
           {displayedCard ? (
             <div className="mt-6 space-y-3">
               {/* Main Bet Button */}
-              {userCard && currentRoom?.gameStatus !== "playing" && currentRoom?.gameStatus !== "ended" && (
+
+              {userCard && currentRoom?.gameStatus !== "playing" && (
                 <button
                 onClick={isBetActive ? handleCancelBet : handlePlaceBet}
                 className={`w-full px-4 py-2 rounded-lg shadow font-semibold transition ${
